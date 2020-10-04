92b54133b902b1b89d804d3dd38219676e283ee3
Yash Garg <71602335+yashgarggithub@users.noreply.github.com>
2020-10-04 08:43:16 +0530

    Update tinyvector.h

--- src/tinyvector.h
+++ src/tinyvector.h
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+//I think this guy has left this account
 #ifndef TINYVECTOR_H
 #define TINYVECTOR_H
 
